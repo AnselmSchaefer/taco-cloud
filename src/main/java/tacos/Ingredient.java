a4e48431cca6924b5e44a8dc555164395647ab7e
Anselm Schaefer <schaefab@basfad.basf.net>
2023-02-23 10:11:04 +0100

    change to Spring Data JDBC with schema.sql

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,15 +1,33 @@
 package tacos;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class Ingredient {
+@Table("")
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
 
-	public final String id;
-	public final String name;
-	private final Type type;
-	
-	public enum Type {
-		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  @Id
+  private String id;
+
+  private String name;
+  private Type type;
+
+  @Override
+	public boolean isNew() {
+		return true;
 	}
+
+  public enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
+
 }
