2d238d501820c5f50be6d47090e3f46ad82d6c9b
Anselm Schaefer <schaefab@basfad.basf.net>
2023-02-26 14:44:58 +0100

    customised login and user registration

--- src/main/java/tacos/SecurityConfig.java
+++ src/main/java/tacos/SecurityConfig.java
@@ -1,19 +1,16 @@
 package tacos;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import tacos.security.User;
+import tacos.security.UserRepository;
 
 
 @Configuration
@@ -26,15 +23,35 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		List<UserDetails> usersList = new ArrayList<>();
-		usersList.add(new User(
-				"buzz", encoder.encode("password"),
-					Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-		usersList.add(new User(
-				"woody", encoder.encode("password"),
-					Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-		return new InMemoryUserDetailsManager(usersList);
-				
+	public UserDetailsService userDetailsService(UserRepository userRepo) {
+		return username -> {
+			User user = userRepo.findByUsername(username);
+			if(user != null) return user;
+			
+			throw new UsernameNotFoundException("User '" + username + "' not found");
+		};
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeRequests()
+				.antMatchers("/design", "/orders").hasRole("USER")
+				.anyRequest().permitAll()
+			.and()
+				.formLogin()
+					.loginPage("/login")
+			// Make H2-Console non-secured; for debug purposes
+			.and()
+	        	.csrf()
+	        		.ignoringAntMatchers("/h2-console/**")
+	        // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+	        .and()  
+	        .headers()
+	          .frameOptions()
+	            .sameOrigin()
+	            
+	       .and()
+	       .build();
 	}
 }
